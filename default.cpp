233f298a752f83304cadd2b468caaa3c85b8efc9
smathewihweroush <121179514+smathewihweroush@users.noreply.github.com>
2025-11-11 14:43:27 +0100

    Enhance default.cpp with additional macros
    Added various macros for mathematical and array operations.

--- default.cpp
+++ default.cpp
@@ -18,15 +18,34 @@
 
 // some basic ahh for macros
 #define FORT for(int tii=0;tii<t;tii++)
-#define PRINTC(arr, si) for(int ari=0;ari<si;ari++) cout<<arr[ari]<<' '; cout<<endl
+#define REP(reps, var) for(int var=0;var<reps;var++)
+#define INP(lis, n) for(int i=0;i<n;i++)cin>>lis[i]
+
+// idk mafs? probs
+#define EVEN(x) ((x % 2) == 0)
+#define ODD(x) ((x % 2) == 1)
+#define POS(x) (x > 0)
+#define POS0(x) (x >= 0) // these two just feel clean to me
+#define NEG(x) (x < 0)
+#define NEG0(x) (x <= 0)
+#define DIV(x, y) ((x % y) == 0)
+
+// array related
+#define HAS(l, i) (l[i] != 0)
+#define INRAN(l, r, x) ((l <= x) && (x < r)) // this is inclusive
+#define FILL(l, x, n) for(int i=0;i<n;i++)l[i]=x // general fill
+#define CLEAR(arr) memset(arr, 0, sizeof arr)
+#define PRINTARR(arr, n) for(int i=0;i<n;i++){cout<<arr[i]<<' ';}cout<<endl
 
 using namespace std;
-typedef long long ll;
-typedef std::pair<int, int> pii;
-
+const int MAXN = 200002;
 const int mod = 1000000007;
 const int huge = 1000000000;
 const int big = 20000000;
+
+typedef long long ll;
+typedef std::pair<int, int> pii;
+typedef std::pair<int, int> pll;
 
 int t, n;
 
@@ -41,6 +60,7 @@ int main()
     FORT {
         cin >> n;
     }
+
 
     return 0;
 }
